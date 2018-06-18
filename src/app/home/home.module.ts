d57f2fe450e76661163b33fb8b5149e13f4ade90
Glendon Small <gsmall@sonovate.com>
2018-06-19 00:01:23 +0100

    Modal Set up
    Search Trello to review modal source

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home/home-routing.module'; 
 import { HomeComponent } from './home/home.component';
+import { NgxSmartModalModule, NgxSmartModalService } from 'ngx-smart-modal';
+
 
 @NgModule({
   imports: [
     CommonModule,
+    NgxSmartModalModule.forRoot(),
     HomeRoutingModule  
   ],
   declarations: [ HomeComponent ]
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,18 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 import { User } from '../../_models';
 import { UserService } from '../../_services';
 
 @Component({templateUrl: 'home.component.html'})
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
     users: User[] = [];
-
-    constructor(private userService: UserService) {}
+ 
+    constructor(private userService: UserService, public ngxSmartModalService: NgxSmartModalService) {}
 
     ngOnInit() {
+        
+       
         this.userService.getAll().pipe(first()).subscribe(users => { 
             this.users = users; 
        });
+      
+    
     }
+    
+      ngAfterViewInit() {
+   
+    const obj: Object = {
+      prop1: 'test',
+      prop2: true,
+      prop3: [{a: 'a', b: 'b'}, {c: 'c', d: 'd'}],
+      prop4: 327652175423
+    };
+
+    this.ngxSmartModalService.setModalData(obj, 'myModal');
+          
+          const obj: Object = {
+      prop1: 'test',
+      prop2: true,
+      prop3: [{a: 'a', b: 'b'}, {c: 'c', d: 'd'}],
+      prop4: 327652175423
+    };
+
+    this.ngxSmartModalService.setModalData(obj, 'myModal2');
+            //this.ngxSmartModalService.getModal('myModal2').open(); open on load
+  }
 }
